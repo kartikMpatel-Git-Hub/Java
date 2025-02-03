44782e957cfa20676e8bded913e1fabcf89c296d
kartikMpatel <kartikpatel7892@gmail.com>
2025-02-03 22:59:57 +0530

    .

--- src/Basic_Java/enums/Enum.java
+++ src/Basic_Java/enums/Enum.java
@@ -9,15 +9,20 @@ public class Enum {
         Week(){
             System.out.println("Constructor " + this);
         }
+
+//        void display(); Abstract Are Not Allowed
+        void display(){
+            System.out.println("Display !");
+        }
     }
 
     public static void main(String[] args) {
         Week w = Week.Monday;
-
-        System.out.println(w.ordinal());//It Will Return The Index Of Constrain
-        System.out.println(Week.valueOf("Monday"));//It Will Return The Value Of Argument
-        for(Week ele : Week.values()){
-            System.out.println(ele);
-        }
+        w.display();
+//        System.out.println(w.ordinal());//It Will Return The Index Of Constrain
+//        System.out.println(Week.valueOf("Monday"));//It Will Return The Value Of Argument
+//        for(Week ele : Week.values()){
+//            System.out.println(ele);
+//        }
     }
 }
